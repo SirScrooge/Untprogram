31ad1592d7f355b2c47dd49cf2adb714b53e1c3b
HellPwnage1337 <hellpwnage1337@gmail.com>
2016-11-10 16:31:46 -0600

    Update shell.c

--- shell.c
+++ shell.c
@@ -3,6 +3,15 @@
 #include<string.h>
 int main(int argc, char *argv[])
 {
-	
-	
+	int i=0;
+	char *string;
+	char *input[100];
+	printf("Prompt>	");
+	scanf("%s",string);			//scans user input
+	while(string!=NULL)
+	{
+		input[i]=strtok(string,";");		//puts substrings from string into input commands
+		i++;
+	}	
+}	
   
