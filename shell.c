d003570eb8ed2bc6627caf77c89cbd2c00faef8e
HellPwnage1337 <hellpwnage1337@gmail.com>
2016-11-12 20:50:04 -0600

    Update shell.c

--- shell.c
+++ shell.c
@@ -2,8 +2,8 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
-int token(char *string);
-int main(int argc, char *argv[])
+int pip(char *input);
+int main()
 {
 	//Check if program is to run batch mode
 	
@@ -12,27 +12,87 @@ int main(int argc, char *argv[])
 	int i=0;
 	int j=0;
 	char *string;
-	char *input[100];
-	if(argc==1)
+	char *input;
+	char *p;
+	//char tok[100];
+	while(1)
 	{
+		
 		printf("Prompt> ");
-    		scanf("%s",string);                     //scans user input
-		token(string);
-		for(j=0;j<i;j++)	//executes commands
+    	scanf("%s",string);                     //scans user input
+		input=strtok(string,";");  		//puts substrings from string into input commands
+		//i=0;
+		/*while((getchar(input))!=NULL)
 		{
-			char *arg[]={input[j],NULL};
-			execvp(input[j],arg);
+			tok[i]=getchar(input);
+		}*/
+		while(input!=NULL)	//executes commands
+		{
+			/*i=0;
+			while(tok[i]!="|"||tok[i]!=NULL||tok[i]!=";")
+			{
+				if(tok[i]=="|")
+				{
+					pip(input);
+					break;
+				}
+				else
+				{
+					i++;
+				}	
+			}		*/
+			//if(fork()==0)
+		//	{	
+			if(input=="quit\n")
+			{
+				exit(1);
+			}	
+			char *arg[]={input,NULL};
+			execvp(input,arg);
+			input=strtok(NULL,";");
+		//	}	
+			
 		}
+
 	}	
 }  
-int token(char *string)
+/*int pip(char *input)
 {
-	printf("Prompt> ");
-        scanf("%s",string);                     //scans user input
-        // entering something returns a segmentation fault
-        while(string!=NULL)
-        {
-           input[i]=strtok(string,";");            //puts substrings from string into input commands
-           i++;
-        }
+	int fd[2];
+	pipe(fd);	//creates a pipe
+	char *cmd[2];
+	cmd[0]=strtok(input,"|");
+	cmd[1]=strtok(NULL,"|");
+	if (fork()==0)
+	{
+		
+		dup2(fd[1],1); //puts output into pipe
+		close(fd[0]);
+		close(fd[1]);
+		
+		char *arg[]={cmd[0],NULL};
+		execvp(cmd[0],arg);	//does the first argument
+		exit(1);
+	}
+	else if(fork()==0)
+	{
+		dup2(fd[0],0);		//reads the input from pipe
+		close(fd[0]);
+		close(fd[1]);
+		char *arg[]={cmd[1],NULL};
+		execvp(cmd[1],arg);	//does 2nd argument	
+		exit(1);
+	}
+	else 
+	{
+		
+		close(fd[0]);
+		close(fd[1]);
+		
+		wait();
+		wait();
+		printf("command line pipe completed.\n");
+	}
+
 }	
+*/
