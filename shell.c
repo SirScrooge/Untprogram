a8155a86cb56a66797e4c06c57326d54dcce6124
NicholasPartridge <nicholaspartridge@my.unt.edu>
2016-11-12 22:54:12 -0600

    Add files via upload

--- shell.c
+++ shell.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <string.h>  //strtok()
-#include <signal.h> //ctrl-c quit
 
 #define MAX_LENGTH 512  //max length for a single command to 512 characters
 #define READ 0          //read index
@@ -11,54 +9,53 @@
 
 int main(int argc, char *argv[]){
 
-int fd[2];                //file descriptor variable
 char * token;     //temporary pointer to tokenize command for processing
 char  buffer[MAX_LENGTH]; //parent processing buffer
-//char * temp_cmd[100];
 
-    pipe(fd);   //create pipe in parent
 
-    if (argc > 2){
+    if (argc > 2){	 //if wrong # of arguments supplied, exit the shell
         printf("Usage: ./shell [batchFile] \nerror- too many arguments (%i)!\n", argc);
         exit(0);
     }
-    else if (argc == 1){
+    else if (argc == 1){ //if shell is to run in interactive mode
         printf("*** interactive mode ***\n");
 
-        while (1){
+        while (1){ 	 //run continuously until quit is signaled
             printf("prompt> ");
+            fgets (buffer, sizeof(buffer), stdin); //grab the string from the command line
 
-            fgets (buffer, sizeof(buffer), stdin);
-            token = strtok (buffer, ";\n");
-
-            while (token != NULL){
+	    token = strtok (buffer, ";\n");	//tokenize until ; or \n is found
+      	    if (strcmp(token, "quit") == 0){	//if user enters quit, exit the shell
+		exit(1);
+	    }//nested if
+            while (token != NULL){	//while there are still tokens to be run, continue
                 system(token);
-                token = strtok(NULL, ";");
-            }
-        }
-    }
-    else if (argc == 2){
+                token = strtok(NULL, ";"); //remove the previous token
+            }//2nd while
+        }//1st while
+    }//else if
+    else if (argc == 2){	//if a batch file is supplied, run batch mode
 	FILE *infile = fopen(argv[1], "r");
 
         if(infile == NULL){
 	    perror("Error ");
 	    return 1;
-	}
+	}//if
+	printf ("*** batch mode ***\n");
+        char line[MAX_LENGTH];
 
-        char line[120];
+   	while(fgets(line, sizeof(line), infile)){	//grab each line for processing
+            printf("\nbatch line> %s\n", line);
+            token = strtok(line, ";\n");	//tokenize until ; or \n is found
 
-   	 while(fgets(line, sizeof(line), infile)){
-            printf("batch line> %s\n", line);
-            token = strtok(line, ";\n");
-
-	    while (token != NULL){
+	    while (token != NULL){	//while the token is still valid
                 system(token);
-                token = strtok(NULL, ";\n");
-            }
-         }
+                token = strtok(NULL, ";\n");	//remove previous token
+            }//2nd while
+        }//1st while
 
-    }
+    }//else if
 
     return 0;
-}
+}//main
 
