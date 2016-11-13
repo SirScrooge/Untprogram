71deaa0ebf637933f53baf64c68be629003ee4cc
NicholasPartridge <nicholaspartridge@my.unt.edu>
2016-11-12 21:04:17 -0600

    Update NPshell.c

--- NPshell.c
+++ NPshell.c
@@ -1,4 +1,3 @@
-//Shell.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,54 +5,40 @@
 #include <string.h>  //strtok()
 #include <signal.h> //ctrl-c quit
 
-#define MAX_LENGTH 512 	//max length for a single command to 512 characters
-#define READ 0		//read index
-#define WRITE 1 	//write index
+#define MAX_LENGTH 512  //max length for a single command to 512 characters
+#define READ 0          //read index
+#define WRITE 1         //write index
 
-void run_batch();
-void run_interactive();
-	
 int main(int argc, char *argv[]){
 
-int fd[2];		  //file descriptor variable
-char * token; 	  //temporary pointer to tokenize command for processing
+int fd[2];                //file descriptor variable
+char * token;     //temporary pointer to tokenize command for processing
 char  buffer[MAX_LENGTH]; //parent processing buffer
 //char * temp_cmd[100];
 
-    pipe(fd);	//create pipe in parent
+    pipe(fd);   //create pipe in parent
 
     if (argc > 2){
-	printf("Usage: ./shell [batchFile] \nerror- too many arguments (%i)!\n", argc);
-	exit(0);
+        printf("Usage: ./shell [batchFile] \nerror- too many arguments (%i)!\n", argc);
+        exit(0);
     }
-    else if(argc == 2)
-    {
-	run_batch();
-    }
-    else if (argc == 1)
-    {
-	run_interactive();	
+    else if (argc == 1){
+        printf("*** interactive mode ***\n");
+
+        while (1){
+            printf("prompt> ");
+
+            fgets (buffer, MAX_LENGTH, stdin);
+            token = strtok (buffer, ";\n");
+
+            while (token != NULL){
+                printf("\n*** running command: %s ***\n", token);
+                system(token);
+                token = strtok(NULL, ";");
+            }
+        }
     }
 
     return 0;
 }
 
-void run_batch()
-{
-	printf("*** batch mode ***\n");
-}
-
-void run_interactive()
-{
-        printf("*** interactive mode ***\n");
-
-	while (1){
-	    printf("prompt> ");
-
-	    fgets (buffer, MAX_LENGTH, stdin);
-	    token = strtok (buffer, ";\n");
-	    while (token != NULL){
-
-	    }
-        }	
-}
